2d1e43da246050c03a115a0e3421293bafc040fb
sandun.m <sandun.madampage@scicom.com.my>
2021-06-05 22:25:54 +0530

    depl: innitialize for deployment

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-    return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
-
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(requireHTTPS);
-app.use(express.static('./dist/tutor-x-frontend'));
+// Serve only the static files form the dist directory
+// Replace the '/dist/<to_your_project_name>'
+app.use(express.static(__dirname + '/dist/tutor-x-frontend'));
 
-app.get('/*', (req, res) =>
-  res.sendFile('./dist/tutor-x-frontend/index.html'),
-);
-
+app.get('*', function(req,res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(__dirname + '/dist/tutor-x-frontend/index.html'));
+});
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
