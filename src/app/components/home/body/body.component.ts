c8ad9341b2bfc8f745b2547f3ec535368368835f
DeepthaShash <shashideepshashi@outlook.com>
2021-06-14 22:26:31 +0530

    feat: created client container in home page

--- src/app/components/home/body/body.component.ts
+++ src/app/components/home/body/body.component.ts
@@ -9,6 +9,8 @@ export class BodyComponent implements OnInit {
 
   constructor() { }
 
+  selectedNumber = 0;
+
   ngOnInit(): void {
   }
 
@@ -20,5 +22,15 @@ export class BodyComponent implements OnInit {
     {title:"Flexibility", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum"},
     {title:"User friendly", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum"}
   ]
+
+  feedback = [
+    {title1:"Incredible Experience", title2:"Dependable, Responsive, Professional Partner",content1: "We had an incredible experience working with Landify and were impressed they made such a big difference in only three weeks. Our team is so grateful for the wonderful improvements they made and their ability to get familiar with the concept so quickly. It acted as a catalyst to take our design to the next level and get more eyes on our product.",content2:"Fermin Apps has collaborated with Landify team for several projects such as Photo Sharing Apps and Custom Social Networking Apps. The experience has been pleasant, professional and exceeding our expectations. The team is always thinking beyond the current tasks & helps us formulate a vision and possibilities for future.",name1:"Jane Cooper", name2:"Jane Cooper", designation1:"CEO, ABC Corporation", designation2:"CEO, ABC Corporation",image1:"../../../../assets/images/Logo.png", image2:"../../../../assets/images/Logo.png"},
+    {title1:"Very Helpfull", title2:"Highly Appreceated",content1: "We had an incredible experience working with Landify and were impressed they made such a big difference in only three weeks. Our team is so grateful for the wonderful improvements they made and their ability to get familiar with the concept so quickly. It acted as a catalyst to take our design to the next level and get more eyes on our product.",content2:"Fermin Apps has collaborated with Landify team for several projects such as Photo Sharing Apps and Custom Social Networking Apps. The experience has been pleasant, professional and exceeding our expectations. The team is always thinking beyond the current tasks & helps us formulate a vision and possibilities for future.",name1:"Jane Cooper", name2:"Jane Cooper", designation1:"CEO, ABC Corporation", designation2:"CEO, ABC Corporation", image1:"../../../../assets/images/Logo.png", image2:"../../../../assets/images/Logo.png"},
+    {title1:"Incredible Experience", title2:"Dependable, Responsive, Professional Partner",content1: "We had an incredible experience working with Landify and were impressed they made such a big difference in only three weeks. Our team is so grateful for the wonderful improvements they made and their ability to get familiar with the concept so quickly. It acted as a catalyst to take our design to the next level and get more eyes on our product.",content2:"Fermin Apps has collaborated with Landify team for several projects such as Photo Sharing Apps and Custom Social Networking Apps. The experience has been pleasant, professional and exceeding our expectations. The team is always thinking beyond the current tasks & helps us formulate a vision and possibilities for future.",name1:"Jane Cooper", name2:"Jane Cooper", designation1:"CEO, ABC Corporation", designation2:"CEO, ABC Corporation", image1:"../../../../assets/images/Logo.png", image2:"../../../../assets/images/Logo.png"}
+  ]
+
+  change(num: number){
+    this.selectedNumber = num;
+  }
 
 }
