498149bce7e06ba42ec5f29157a8bb010ebd004a
Deeptha Shashimina <deeptha.s@scicom.com.my>
2021-08-02 19:31:43 +0530

    fix: few changes in the chat

--- src/app/components/shared/chat/chat.component.ts
+++ src/app/components/shared/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewChecked, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {FormControl} from "@angular/forms";
 
 @Component({
@@ -6,15 +6,28 @@ import {FormControl} from "@angular/forms";
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements OnInit {
-
+export class ChatComponent implements OnInit, AfterViewChecked {
+  // @ts-ignore
+  @ViewChild('scrollMe') private myScroll: ElementRef;
   message = new FormControl('');
+  messages:any = [];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewChecked() {
+    this.scrollToBottom();
+  }
+
   onSend(){
-    console.log(this.message.value);
+    this.messages.push(this.message.value);
+    this.message.reset();
+  }
+
+  scrollToBottom(): void {
+    try {
+      this.myScroll.nativeElement.scrollTop = this.myScroll.nativeElement.scrollHeight;
+    } catch(err) { }
   }
 }
