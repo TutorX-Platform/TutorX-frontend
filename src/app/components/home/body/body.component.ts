d9adee013a023818874470eb6bbae7e96e43b04e
tharindupr <tharindu.prf@gmail.com>
2021-08-20 00:56:24 +0100

    tutor profile added

--- src/app/components/home/body/body.component.ts
+++ src/app/components/home/body/body.component.ts
@@ -181,91 +181,85 @@ export class BodyComponent implements OnInit {
     {
       title: "Incredible Experience",
       content: "I'm really impressed and grateful to \"tutetory.com\" for helping me to score higher for my python assignment series. These tutors have a good subject mastery and are responsive to my texts anytime. I learned a lot of things which I will never be able to learn during my class practical sessions",
-      name: "Jane Cooper",
+      name: "Jane C.",
       designation: "Florida",
-      image: "../../../../assets/images/profile.jpg",
+      image: "../../../../assets/images/default_profile.png",
     },
     {
       title: "Smooth workflow",
       content: "One of the most outstanding tutoring sites I had worked with. Genius tutors with perfect answers. How easy it is to post my questions to the platform with no hassle at all. Smooth and fast workflow. So excited to get connected again.",
-      name: "Jordan Anderson",
+      name: "Jordan A.",
       designation: "Ohio",
-      image: "../../../../assets/images/profile.jpg",
+      image: "../../../../assets/images/default_profile.png",
     },
     {
       title: "Fast Delivery",
       content: "Captain is a true genius. He always finished the work far earlier than the deadline. Could complete my semester with flying colours. I never leave any feedback, but this is going to be the only exception.  The tutor literary saved my life. ",
-      name: "Flynn Sullivan",
+      name: "Flynn S.",
       designation: "Belfast",
-      image: "../../../../assets/images/profile.jpg",
+      image: "../../../../assets/images/default_profile.png",
     },
     {
       title: "Excellent Support",
       content: "I was originally skeptical because my deadline was coming up and the codes were not working, but this tutor will go above and beyond to make sure the assignment specifications are met. Absolutely best set of tutors and support team ever!! ",
       name: "Hanna B.",
       designation: "Texas",
-      image: "../../../../assets/images/profile.jpg",
+      image: "../../../../assets/images/default_profile.png",
     },
     {
       title: "Clear Explanations",
       content: "Recommend tutetory.com to anyone who is struggling for Matlab problems. Explained the code one by when I requested. They have a genius group of tutors. I could get the help of few and all are world-class. Never hesitate to come back. Cheers!",
-      name: "Ravi Mishra",
+      name: "Ravi M.",
       designation: "Toronto",
-      image: "../../../../assets/images/profile.jpg",
+      image: "../../../../assets/images/default_profile.png",
     },
     {
       title: "Affordable Prices",
       content: "Best tutoring site I came across. I worked with Captain and Mechanic. Both were highly concerning about timely submissions.I could get all clarifications whenever i asked. No any cost ,they were always available .Really appreciate the hard work. Most trusted tutoring  site",
-      name: "Sami Amar",
+      name: "Sami A.",
       designation: "London",
-      image: "../../../../assets/images/profile.jpg",
+      image: "../../../../assets/images/default_profile.png",
     },
   ]
 
   tutorFeedbacks = [
     {
-      description: "A computer science PhD candidate and a former software engineer specialized in Databases, Algorithms, Blockchain and Machine Learning and programming languages like Python, Java, C and JavaScript",
-      image: "assets/images/profile.jpg",
+      description: "I’m a computer science PhD candidate and former software engineer. My expertise’s are on Databases, Algorithm, Machine Learning and Blockchain. I have industry experience in numerous programming languages which includes Python, C, C++, Java and JavaScript. Waiting to share my experience with you !!",
+      image: "assets/images/expertcs.jpg",
       name: "ExpertCSTutor",
       subject: "Computer Science"
     },
     {
-      description: "BSc. Electronic and Electrical Engineering degree holder having 8+ experience in the telecommunication space. An expert in Mathematics, Physics, IP networking, Digital Electronics and Telecommunication",
-      image: "assets/images/profile.jpg",
+      description: "Masters degree holder and PhD candidate in Mechanical Engineering. Specialized in Mathematics, Fluid Dynamics and Heat flow.................................................................",
+      image: "assets/images/mechanic.jpg",
+      name: "Mechanic",
+      subject: "Mechanical Engineering"
+    },
+    {
+      description: "Hi, I am a passionate Electrical and Electronic Engineering graduate having a sound academic knowledge. Currently I am working as a IP Core Network Engineer in a leading telecommunication company. I provide tutoring on subjects such as IP Networking, Electrical and Power, Telecommunications, Digital Electronics, Mathematics and Physics.",
+      image: "assets/images/captain.jpg",
       name: "Captain",
-      subject: "Computer Science"
+      subject: "Electronic Engineering"
     },
     {
-      description: "Masters degree holder and PhD candidate in Mechanical Engineering. Specialized in Mathematics, Fluid Dynamics and Heat flow",
-      image: "assets/images/profile.jpg",
-      name: "Mechanic",
-      subject: "Mechanical Engineering"
+      description: "I am an M.Phil post graduate in Economics.. I am currently doing my PhD in Economics and the topic of my doctoral thesis is Crypto Economics. I am very much interested in helping out students online, both with their homework and tutoring them when required. My areas of specialization are Economics, business statistics and business management subjects.",
+      image: "assets/images/hr.jpg",
+      name: "Businesswoman ",
+      subject: "Business Management"
     },
     {
-      description: "Masters degree holder and PhD candidate in Mechanical Engineering. Specialized in Mathematics, Fluid Dynamics and Heat flow",
-      image: "assets/images/profile.jpg",
-      name: "Mechanic",
-      subject: "Mechanical Engineering"
-    },
-    {
-      description: "Masters degree holder and PhD candidate in Mechanical Engineering. Specialized in Mathematics, Fluid Dynamics and Heat flow",
-      image: "assets/images/profile.jpg",
-      name: "Mechanic",
-      subject: "Mechanical Engineering"
+      description: "Let me start by saying that I believe, teaching is a noble profession which should be done with passion. I have a good academic record and great experience in teaching in the subject Chemistry through last 5 years. I have completed my Ph.D. in 2020. I’m specialized in both organic and inorganic Chemistry.",
+      image: "assets/images/h2o.jpg",
+      name: "Prof. H2O",
+      subject: "Chemistry"
     },
   ]
 
   tutorFeedbacks2 = [
     {
-      description: "A computer science PhD candidate and a former software engineer specialized in Databases, Algorithms, Blockchain and Machine Learning and programming languages like Python, Java, C and JavaScript",
-      image: "assets/images/profile.jpg",
+      description: "I’m a computer science PhD candidate and former software engineer. My expertise’s are on Databases, Algorithm, Machine Learning and Blockchain. I have industry experience in numerous programming languages which includes Python, C, C++, Java and JavaScript. Waiting to share my experience with you !!",
+      image: "assets/images/expertcs.jpg",
       name: "ExpertCSTutor",
-      subject: "Computer Science"
-    },
-    {
-      description: "BSc. Electronic and Electrical Engineering degree holder having 8+ experience in the telecommunication space. An expert in Mathematics, Physics, IP networking, Digital Electronics and Telecommunication",
-      image: "assets/images/profile.jpg",
-      name: "Captain",
       subject: "Computer Science"
     },
     {
@@ -273,6 +267,12 @@ export class BodyComponent implements OnInit {
       image: "assets/images/profile.jpg",
       name: "Mechanic",
       subject: "Mechanical Engineering"
+    },
+    {
+      description: "Hi, I am a passionate Electrical and Electronic Engineering graduate having a sound academic knowledge. Currently I am working as a IP Core Network Engineer in a leading telecommunication company. I provide tutoring on subjects such as IP Networking, Electrical and Power, Telecommunications, Digital Electronics, Mathematics and Physics.",
+      image: "assets/images/captain.jpg",
+      name: "Captain",
+      subject: "Electronic Engineering"
     }
   ]
 
